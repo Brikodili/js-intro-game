f2aa52daf6b9c559aba78568be5049df8ad78dae
Anna Brikodili <brikodili.troyan@gmail.com>
2018-08-07 10:36:56 +0300

    refactoring

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var start = confirm("Начать игру?");
+var start = confirm('Start?');
 
 if (start === true) {
     checkNum();
@@ -7,19 +7,24 @@ if (start === true) {
 }
 
 function checkNum() {
-    var c;
+    var c; // coefficient
     var money = 0;
+
     for (c = 1; c > 0; c *= 3) {
         var randomNum = Math.floor( Math.random() * 5) + 1;
-        console.log(randomNum);
+        console.log(randomNum); // для проверки
+
         var i = 1;
+
         while (i < 4) {
-            var userNum = +prompt("Введите число от 1 до 5", "");
+            var userNum = +prompt('Enter a number between 1 and 5', '');
+
             if (userNum === randomNum) break;
             else {
                 i++;
             }
         }
+
         switch (i) {
             case 1:
                 money += 10 * c;
@@ -37,12 +42,14 @@ function checkNum() {
             default:
                 alert('Error');
         }
-        console.log(money);
-        if (i <= 3){
-            var continueGame = confirm('continue?');
+        console.log('Your winnings - ' + money);
+
+        if (i <= 3) {
+            var continueGame = confirm('Continue?');
+
             if (continueGame === false) {
                 c = -1;
-                console.log('Thanks');
+                console.log('Thanks for the game!');
                 break;
             }
         }
